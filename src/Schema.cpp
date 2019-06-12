ebc5b92ed1beef91aed7bf37d96597e22dc3477b
Vagrant Default User <vagrant@stretch.localdomain>
2019-06-12 01:15:54 +0000

    carpteta modulos movida a src

--- src/Schema.cpp
+++
@@ -1,7 +0,0 @@
-#include "Schema.h"
-
-Schema(string id,)
-{
- string id;
-
-}
---
+++ src/mind.cpp
@@ -0,0 +1,47 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+//
+// A tutorial on how to use the Cartesian Interface to control a limb
+// in the operational space.
+//
+// Author: Ugo Pattacini - <ugo.pattacini@iit.it>
+
+#include <yarp/os/Network.h>
+
+#include <jsoncpp/json/json.h>
+
+#include <yarp/os/impl/Logger.h>
+
+#include <fstream>
+#include <iostream>
+#include <emotion.cpp>
+
+using namespace std;
+using namespace yarp::os;
+using namespace yarp::os::impl;
+
+
+int main(int argc, char *argv[])
+{
+    Network yarp;
+
+    if (!yarp.checkNetwork())
+    {
+        yError("Yarp server does not seem available\n");
+        return 1;
+    }
+
+    Emotions emotions;
+    Memory memory;
+    iDevER idever;
+    Perception perception;
+    bodyController bodyController;
+
+    ResourceFinder rf;
+    rf.setVerbose(); //logs searched directories
+    rf.setDefaultConfigFile("config.ini"); //specifies a default configuration file
+    rf.configure(argc,argv);
+
+    memory.runModule(rf);
+    // return 0;
+}
+
