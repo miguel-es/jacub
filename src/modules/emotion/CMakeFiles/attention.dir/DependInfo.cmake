e05e9716c0808106a4e0f11eb0b09e2b2a27e257
Miguel Estrada <luism@iib.unam.mx>
2022-04-22 14:22:14 -0500

    CMakeFiles

---
+++ src/modules/emotion/CMakeFiles/attention.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/miguel/eclipse-workspace/jacub/src/utils/jutils.cpp" "/home/miguel/eclipse-workspace/jacub/src/modules/attention/CMakeFiles/attention.dir/home/miguel/eclipse-workspace/jacub/src/utils/jutils.o"
+  "/home/miguel/eclipse-workspace/jacub/src/utils/schemaUtils.cpp" "/home/miguel/eclipse-workspace/jacub/src/modules/attention/CMakeFiles/attention.dir/home/miguel/eclipse-workspace/jacub/src/utils/schemaUtils.o"
+  "/home/miguel/eclipse-workspace/jacub/src/modules/attention/src/AttentionModule.cpp" "/home/miguel/eclipse-workspace/jacub/src/modules/attention/CMakeFiles/attention.dir/src/AttentionModule.o"
+  "/home/miguel/eclipse-workspace/jacub/src/modules/attention/src/main.cpp" "/home/miguel/eclipse-workspace/jacub/src/modules/attention/CMakeFiles/attention.dir/src/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "../../../src/utils"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/modules/emotion/CMakeFiles/attention.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/attention.dir/src/AttentionModule.o"
+  "CMakeFiles/attention.dir/src/main.o"
+  "CMakeFiles/attention.dir/home/miguel/eclipse-workspace/jacub/src/utils/jutils.o"
+  "CMakeFiles/attention.dir/home/miguel/eclipse-workspace/jacub/src/utils/schemaUtils.o"
+  "attention.pdb"
+  "attention"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/attention.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
